5dbb3846a7f08e45cd2deab129c625bcdb25bcd1
Suvrodip Chakroborty <suvrodipchakroborty01@gmail.com>
2026-01-28 00:24:11 +0530

    first commit

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || true,
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true, // Important: Allows cookies to be sent/received
   headers: {
     'Content-Type': 'application/json',
--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: process.env.CLIENT_URL,
+  origin: true,
   credentials: true,
   methods: ['GET','POST','PUT','DELETE','OPTIONS'],
   allowedHeaders: ['Content-Type','Authorization'],
