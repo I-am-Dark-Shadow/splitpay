2918f2f25ad5089cac5989a3591e3b2a9c963c68
Suvrodip Chakroborty <suvrodipchakroborty01@gmail.com>
2026-01-28 00:28:10 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: true,
+  origin: process.env.CLIENT_URL,
   credentials: true,
   methods: ['GET','POST','PUT','DELETE','OPTIONS'],
   allowedHeaders: ['Content-Type','Authorization'],
